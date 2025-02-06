4f0038319126d23f8973bbb8eb4e0c0ac2acd79d
Ezra Mechaber <3474812+ezramechaber@users.noreply.github.com>
2025-02-06 10:18:16 -0500

    Posthog install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from 'react';
+import { CSPostHogProvider } from './providers';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,9 +21,11 @@ export default function RootLayout({
 }): React.ReactElement {
   return (
     <html lang="en" className={`${inter.variable} font-sans`}>
+      <CSPostHogProvider>
       <body className="bg-gray-50 text-gray-900 antialiased">
         {children}
       </body>
+      </CSPostHogProvider>
     </html>
   );
 }
